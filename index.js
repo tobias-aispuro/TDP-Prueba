b2768b310d558b002b556e4a50dfece4c9331c4a
tobias-aispuro <tobiasaispuro10@gmail.com>
2024-08-29 21:59:39 -0300

    Cambio index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function suma(a,b) {
     return a+b
 }
+
+const array = [1,2,3,4,5,6,7,8,9]
+const aux = 0;
+
+function coleccion(array) {
+    for (const i = 0; i < array.length-1; i++) {
+        if (array[i] < array[i+1]) {
+          aux = suma(array[i], array[i+1]);
+        }
+    }
+}
+
+console.log(aux);
